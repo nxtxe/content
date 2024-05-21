d0f1d59fdf215ced589f9d1f347d3f26e03eb749
nite <94257293+nxtxe@users.noreply.github.com>
2024-05-21 18:19:45 +0000

    Create giveaway.ts

---
+++ giveaway.ts
@@ -0,0 +1,5 @@
+const randomMemberIndex = Math.floor(Math.random() * giveaway.members.length);
+const randomMember = giveaway.members[randomMemberIndex];
+
+// Не сложнее этого :)
+// No more difficult than this :)
